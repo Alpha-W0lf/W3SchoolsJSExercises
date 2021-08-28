d83c7317e7e5d1c053aa535bdaac4df479f0f411
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-28 12:21:55 -0500

    Update JS_Exercises.js

--- JS_Exercises.js
+++ JS_Exercises.js
@@ -1,6 +1,35 @@
 // JS Variables 1-1: Create a variable called carName, assign the value Volvo to it.
-
 var carName = "Volvo";
-
 document.write(carName);
 console.log(carName);
+
+// JS Variables 2-1: Create a variable called x, assign the value 50 to it.
+
+// JS Variables 3-1: Display the sum of 5 + 10, using two variables: x and y.
+
+// JS Variables 4-1: Create a variable called z, assign x + y to it, and display the result in an alert box.
+
+// JS Variables 5-1: On one single line, declare three variables with the following names and values:
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+
+// JS Operators 1-1: Multiply 10 with 5, and alert the result:
+
+// JS Operators 2-1: Divide 10 by 2, and alert the result:
+
+// JS Operators 3-1: Alert the remainder when 15 is divided by 9.
+
+// JS Operators 4-1: Use the correct assignment operator that will result in x being 15 (same as x = x + y).
+
+// JS Operators 5-1: Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+
+// JS Data Types 1-1: Use comments to describe the correct data type of the following variables:
+
+// JS Functions 1-1: Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+
+// JS Functions 2-1: Create a function called "myFunction".
+
+// JS Functions 3-1: Make the function return "Hello".
+
+// JS Functions 4-1: Make the function display "Hello" in the inner HTML of an element with the ID "demo".
