938001c9a55b2609b3bcd8f31cd2a33846dced05
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-28 13:40:08 -0500

    Update JS_Exercises.js

--- JS_Exercises.js
+++ JS_Exercises.js
@@ -62,3 +62,7 @@
 // };
 // document.getElementById('demo').innerHTML = myFunction();
 // JS Functions 4-1: Make the function display "Hello" in the inner HTML of an element with the ID "demo".
+function myFunction() {
+    document.getElementById('demo').innerHTML = "Hello";
+};
+myFunction();
