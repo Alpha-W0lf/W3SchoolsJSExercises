e07c1c9d304f543a1d7395cf9388ac66819b709f
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-29 10:35:13 -0500

    Update JS_Exercises.js

--- JS_Exercises.js
+++ JS_Exercises.js
@@ -133,7 +133,7 @@
 // console.log(cars[0]);
 // JS Arrays 3-1: Alert the number of items in an array, using the correct Array property.
 const cars = ["Volvo", "Jeep", "Mercedes"];
-alert(length.cars);
+alert(cars.length);
 // JS Array Methods 1-1: Use the correct Array method to remove the last item of the fruits array.
 
 // JS Array Methods 2-1: Use the correct Array method to add "Kiwi" to the fruits array.
