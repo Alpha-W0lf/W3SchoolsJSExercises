8c32825e6eedb5602e017fec43df414eb3e60563
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-28 20:45:05 -0500

    Update JS_Exercises.js

--- JS_Exercises.js
+++ JS_Exercises.js
@@ -81,7 +81,11 @@
 // console.log(person.country);
 // JS Objects 3-1: Create an object called person with name = John, age = 50.
 // Then, access the object to alert("John is 50").
-
+// const person = {
+//     name : "John",
+//     age : 50
+// }
+// alert(person.name + " is " + person.age);
 // JS Events 1-1: The <button> element should do something when someone clicks on it. Try to fix it!
 
 // JS Events 2-1: When the button is clicked, the function "myFunction" should be executed.
