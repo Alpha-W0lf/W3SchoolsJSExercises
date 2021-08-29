ad72867b61b2b00e9f53f8ca9a4e798c012eb68a
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-29 14:28:14 -0500

    Update JS_Exercises.js

--- JS_Exercises.js
+++ JS_Exercises.js
@@ -252,7 +252,11 @@
 //     console.log(x);
 // }
 // JS While Loops 1-1: Create a loop that runs as long as i is less than 10.
-
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++
+};
 // JS While Loops 2-1: Create a loop that runs as long as i is less than 10, but increase i with 2 each time.
 
 // JS Break Loops 1-1: Make the loop stop when i is 5.
