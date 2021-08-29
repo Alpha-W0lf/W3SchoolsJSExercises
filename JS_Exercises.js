ba89b4dd4d08e651a475d6ec3ea62864240ed1d3
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-28 21:06:31 -0500

    Update JS_Exercises.js

--- JS_Exercises.js
+++ JS_Exercises.js
@@ -108,7 +108,9 @@
 // console.log(pos);
 // alert(pos);
 // JS String Methods 2-1: Use the slice method to return the word "bananas".
-
+// let txt = "I can eat bananas all day";
+// let x = txt.slice(10, 17);
+// console.log(x);
 // JS String Methods 3-1: Use the correct String method to replace the word "Hello" with the word "Welcome".
 
 // JS String Methods 4-1: Convert the value of txt to upper case.
